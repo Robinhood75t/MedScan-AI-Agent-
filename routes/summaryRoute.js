625d02187d08d357221d53d79942e820f798e3f0
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-08 13:27:48 +0530

    Protecting summary route

--- routes/summaryRoute.js
+++ routes/summaryRoute.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const summarize = require("../controllers/summayController");
 const upload = require("../middleware/uploadMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/summarize", upload.single("file"), summarize);
+router.use(authMiddleware);
+
+router.post("/summarize", authMiddleware, upload.single("file"), summarize);
 
 module.exports = router;
