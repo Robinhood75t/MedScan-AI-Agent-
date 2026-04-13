fe9dd2f1ab5ca0da2bfb7c638f6cf329a435c7cf
Robinhood75t <robinparjapatikumar@gmail.com>
2026-04-13 11:07:29 +0530

    adding file validation

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -3,7 +3,15 @@ const multer = require("multer");
 
 const upload = multer({
     dest: "uploads/",
-    limits: {fileSize: 5 * 1024 * 1024}
+    limits: {fileSize: 5 * 1024 * 1024},
+    fileFilter: (req, file, cb) => {
+        const allowed = ["application/pdf","image/jpeg","image/png","image/jpg", "image/webp"];
+        if(allowed.includes(file.mimetype)){
+            cd(null, true);
+        }else{
+            cb(new Error("file type not allowed"));
+        }
+    }
 })
 
 module.exports = upload;
