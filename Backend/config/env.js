6a1705a682ecd46ba6d0e53c887bf914b239de28
Robin Kumar <161246802+Robinhood75t@users.noreply.github.com>
2026-04-13 10:07:36 +0530

    Delete Backend directory

--- Backend/config/env.js
+++
@@ -1,6 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-if (!process.env.PERPLEXITY_API_KEY) {
-    throw new Error("PERPLEXITY_API_KEY missing in .env");
-}
--- Backend/middleware/uploadMiddleware.js
+++
@@ -1,9 +0,0 @@
-const multer = require("multer");
-// upload middleware
-
-const upload = multer({
-    dest: "uploads/",
-    limits: {fileSize: 5 * 1024 * 1024}
-})
-
-module.exports = upload;
