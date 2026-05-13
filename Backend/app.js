bfe07e14be904088fa07a0639e627526581adce0
Robinhood75t <robinparjapatikumar@gmail.com>
2026-05-13 21:51:05 +0530

    Backend- cors debug

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,15 @@ const userRoutes = require("./routes/userRoute");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/userRoute");
 const summaryRoutes = require("./routes/summaryRoute");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true
+}))
+
 app.use(cookieParser());
 app.use(express.json());
 
