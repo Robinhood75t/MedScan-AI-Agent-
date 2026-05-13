c859b8f9a95edef25e14b71b10fa1d23c08202fc
Robinhood75t <robinparjapatikumar@gmail.com>
2026-05-13 21:49:34 +0530

    Backend-Cors error

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = require("./app");
-const cors = require("cors");
 const dotenv = require("dotenv");
 require("./config/env");
 
 dotenv.config();
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+
 
 connectDB();
 
