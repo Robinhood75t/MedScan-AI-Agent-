1f2992fadbeb86160378366e3c90dd60e00d93a5
Robinhood75t <robinparjapatikumar@gmail.com>
2026-07-04 14:29:05 +0530

    id-inst

--- Frontend/src/pages/Results.tsx
+++ Frontend/src/pages/Results.tsx
@@ -241,7 +241,7 @@ const Results = () => {
   }
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background" id="results-page">
       <Navbar />
       <div className="container mx-auto px-4 md:px-8 pt-24 pb-16">
         <div className="max-w-3xl mx-auto">
